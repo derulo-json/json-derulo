4344cc6bf960221da95ad410d657636a65772211
Alisa Chan <alisachan.ny@gmail.com>
2019-11-04 16:41:49 -0500

    started AllProducts component and started adding product to store

---
+++ client/components/AllProducts.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+
+export class AllProducts extends Component {
+  render() {
+    return <div />
+  }
+}
+
+const mapStateToProps = state => ({})
+
+const mapDispatchToProps = dispatch => {}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AllProducts)
