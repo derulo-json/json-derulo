1437b6b09f5e65a0b0510a7836e3f8d292bd088b
Yooni Park <yoonikj@gmail.com>
2019-11-07 17:05:58 -0500

    fixed typo

--- server/api/Cart.js
+++ server/api/Cart.js
@@ -3,7 +3,7 @@ const {Cart} = require('../db/models/cart')
 
 router.get('/', async (req, res, next) => {
   try {
-    const carte = await Cart.findAll()
+    const cart = await Cart.findAll()
     res.json(cart)
   } catch (error) {
     next(error)
